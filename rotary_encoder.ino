2d85a6dadb69f6977ff50f7b5fa7a7c825761351
Nathan Marcus <n-marcus@outlook.com>
2020-11-09 15:45:59 +0100

    fixed encoder pin numbers

--- rotary_encoder.ino
+++ rotary_encoder.ino
@@ -7,7 +7,7 @@ MIDI_CREATE_DEFAULT_INSTANCE();
 
 static int table[]={0, 1, -1, 0, -1, 0, 0, 1, 1, 0, 0, -1, 0, -1, 1, 0};
 int* avg;
-int index = 0;
+//int index = 0;
 int smooth = 50;
 byte _val = 0;
 
@@ -16,9 +16,14 @@ byte midiOut = 0, _midiOut = 0;
 int count = 0;
 
 void setup() {
-
+  pinMode(13,OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  digitalWrite(13,HIGH);
+  delay(1000);
   MIDI.begin(MIDI_CHANNEL_OMNI);
   avg = (int *) calloc(smooth, sizeof(int));
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -27,17 +32,23 @@ void loop() {
   //mask state of pins with 3 (11 in binary): 101011 -> 000011
   int val = (PIND >> 2) & 0x3;
   int valForAvg = 0;
+
+  if (digitalRead(2) || digitalRead(3)) { 
+    digitalWrite(13,HIGH);
+  } else {
+    digitalWrite(13,LOW);
+  }
   
   if (val != _val){
     int tableIndex = (_val << 2 | val);
-
+    Serial.println("Change! val:" + String(val));
     count += table[tableIndex];
     _val = val;
     valForAvg = 1;
   }
   
-  avg[index] = valForAvg;
-  index = (index + 1) % smooth;
+//  avg[index] = valForAvg;
+//  index = (index + 1) % smooth;
 
   float sum = 0;
   for (int i = 0; i < smooth; i++){
