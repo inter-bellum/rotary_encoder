e8a0c340d4c575417acd7fe002ee3484003897fc
Nathan Marcus <n-marcus@outlook.com>
2020-11-09 17:07:15 +0100

    added idle LED functionality with HSB

--- LEDs.ino
+++ LEDs.ino
@@ -8,20 +8,32 @@
 // On a Trinket or Gemma we suggest changing this to 1:
 #define LED_PIN    10
 
+
 // How many NeoPixels are attached to the Arduino?
 #define LED_COUNT 26
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+bool isIdle = false; //are we idle?
+bool _isIdle = false; //were we idle?
+int timeOutThreshold = 60000; //this is about 5 minutes in ms
+
+int brights[LED_COUNT];//keep track of the brights of every idle LED
+
+int hues[LED_COUNT]; //keep track of all hues
+
+
+
+
 void initLEDs() {
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(100); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
   for (int i = 0; i < LED_COUNT; i ++) {
-    strip.setPixelColor(i, strip.Color(0,100,255));
+    strip.setPixelColor(i, strip.Color(0, 100, 255));
     strip.show();
-    delay(100);
+    delay(50);
   }
   delay(100);
   strip.clear();
@@ -32,11 +44,18 @@ void initLEDs() {
 
 
 void setLEDs(int midiOut) {
-  float ledRange = int((LED_COUNT * 1) * (float(midiOut) / 127.));
-  //  Serial.println("LED range " + String(ledRange));
+  float ledRange = int((LED_COUNT * 1) * (float(midiOut) / 128.));
+  int colorIntensity = midiOut * 2; //map the midi to 0 - 255 to make it work with the LEDs
   for (int i = 0; i < LED_COUNT; i ++) {
     if (i < ledRange) {
-      strip.setPixelColor(i, strip.Color(0, 100, 255));
+      int r = constrain((colorIntensity - 220) * 7, 0, 255);
+      Serial.println("r = " + String(r));
+      int g = 100;
+      int b = constrain(colorIntensity, 0, 100);
+      if (colorIntensity > 150) {
+        b = 10;
+      }
+      strip.setPixelColor(i, r, b , g );
     } else {
       strip.setPixelColor(i, strip.Color(0, 0, 0));
     }
@@ -46,8 +65,61 @@ void setLEDs(int midiOut) {
 }
 
 void blinkLEDS(int vol) {
-  for (int i = 0; i < LED_COUNT; i ++) {
-    strip.setPixelColor(i, strip.Color(vol * 0.3, vol * 0.5, vol));
+  if (LED_COUNT > 0) {
+    for (int i = 0; i < LED_COUNT; i ++) {
+      strip.setPixelColor(i, strip.Color(vol * 0.3, vol * 0.5, vol));
+    }
+    strip.show();
   }
+}
+
+
+
+void idleLEDs() {
+  //this function animates some idle sparkly stuff when the wheel is not touched for a while
+  for (int i = 0; i < LED_COUNT; i++ ) {
+    brights[i] += int(random(-1.8, 2));
+    brights[i] = constrain(brights[i], 0, 255);
+    //    Serial.print(String(scalingTable[int(brights[i]/2.)]) + ",");
+    Serial.print(String(brights[i]) + ", ");
+    hues[i] += int(random(-100, 100));
+    if (hues[i] < 32768) {
+      hues[i] = 65535;
+    } else if (hues[i] > 65535) {
+      hues[i] = 32768;
+    }
+    strip.setPixelColor(i, strip.ColorHSV(hues[i], 255, int(scalingTable[int(brights[i] / 2.)] * 255.)));
+    //    Serial.print(String(brights[i]) + ",");
+  }
+  Serial.println("");
   strip.show();
+}
+
+bool checkIdle() {
+  //checks if the wheel has been touched in a while
+
+  //  Serial.println("Checking if wheel is idle");
+  bool isIdle = false;
+  //  Serial.println("Sincetouched = " + String(sinceTouched));
+  if (sinceTouched > timeOutThreshold) {
+    //    Serial.println("havent been touched in a while");
+    isIdle = true;
+    if (isIdle != _isIdle) {
+      restartIdle();
+    }
+    idleLEDs();
+  }
+
+  _isIdle = isIdle;
+  return isIdle;
+}
+
+void restartIdle() {
+  Serial.println("Restarting idle ");
+  for (int i = 0; i < LED_COUNT; i++) {
+    brights[i] = 0;
+    hues[i] = int(random(32768, 65535)); //fill everything with random hues between blue and red
+    Serial.println(String(hues[i]) + ", " + String(brights[i]) + " - ");
+  }
+  Serial.println("");
 }
--- rotary_encoder.ino
+++ rotary_encoder.ino
@@ -1,66 +1,134 @@
-#include <MIDI.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define CW 1
 #define CCW 2
 
-MIDI_CREATE_DEFAULT_INSTANCE();
+#define DEBUG
 
-static int table[]={0, 1, -1, 0, -1, 0, 0, 1, 1, 0, 0, -1, 0, -1, 1, 0};
-int* avg;
-//int index = 0;
-int smooth = 50;
-byte _val = 0;
 
-byte midiOut = 0, _midiOut = 0;
 
-int count = 0;
+long cycleCount = 0;
+
+elapsedMillis sinceTouched; //keep track of when the wheel was last touched
+
+
+//forward declare functions
+void encoderIRQ(void);
+volatile uint8_t val = 0;
+volatile bool newVal = false;
+
+static int table[] = {0, 1, -1, 0, -1, 0, 0, 1, 1, 0, 0, -1, 0, -1, 1, 0};
+
+const float scalingTable[] = {
+  0.,       0.000689, 0.002756, 0.0062,   0.011022, 0.017222, 0.0248,   0.033756, 0.044089, 0.0558,   0.068889, 0.083356, 0.0992,   0.102362, 0.110236, 0.11811,
+  0.125984, 0.133858, 0.141732, 0.149606, 0.15748,  0.165354, 0.173228, 0.181102, 0.188976, 0.19685,  0.204724, 0.212598, 0.220472, 0.228346, 0.23622,  0.244094,
+  0.251969, 0.259843, 0.267717, 0.275591, 0.283465, 0.291339, 0.299213, 0.307087, 0.314961, 0.322835, 0.330709, 0.338583, 0.346457, 0.354331, 0.362205, 0.370079,
+  0.377953, 0.385827, 0.393701, 0.401575, 0.409449, 0.417323, 0.425197, 0.433071, 0.440945, 0.448819, 0.456693, 0.464567, 0.472441, 0.480315, 0.488189, 0.496063,
+  0.503937, 0.511811, 0.519685, 0.527559, 0.535433, 0.543307, 0.551181, 0.559055, 0.566929, 0.574803, 0.582677, 0.590551, 0.598425, 0.606299, 0.614173, 0.622047,
+  0.629921, 0.637795, 0.645669, 0.653543, 0.661417, 0.669291, 0.677165, 0.685039, 0.692913, 0.700787, 0.708661, 0.716535, 0.724409, 0.732283, 0.740157, 0.748031,
+  0.755906, 0.76378,  0.771654, 0.779528, 0.787402, 0.795276, 0.80315,  0.811024, 0.818898, 0.826772, 0.834646, 0.84252,  0.850394, 0.858268, 0.866142, 0.874016,
+  0.88189,  0.889764, 0.897638, 0.905512, 0.913386, 0.92126,  0.929134, 0.937008, 0.944882, 0.952756, 0.96063,  0.968504, 0.976378, 0.984252, 0.992126, 1.
+};
+
+uint8_t i = 0;
+const uint8_t smooth = 70;
+uint16_t avg[smooth];
+volatile byte _val = 0;
+
+
+const uint8_t midiSmoothCount = 15;
+uint8_t midiSmooth[midiSmoothCount];
+uint8_t midiSmoothIndex = 0;
+uint8_t midiOut = 0, _midiOut = 0;
+
+volatile int count = 0;
+
+const byte inputPin1 = 2;
+const byte inputPin2 = 3;
 
 void setup() {
-  pinMode(13,OUTPUT);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
-  digitalWrite(13,HIGH);
-  delay(1000);
-  MIDI.begin(MIDI_CHANNEL_OMNI);
-  avg = (int *) calloc(smooth, sizeof(int));
-  Serial.begin(9600);
+  NVIC_ENABLE_IRQ(IRQ_PORTD);
+  pinMode(inputPin1, INPUT);
+  attachInterrupt(digitalPinToInterrupt(inputPin1), encoderIRQ, CHANGE);
+  pinMode(inputPin2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(inputPin2), encoderIRQ, CHANGE);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(250);
+  initLEDs();
+
+#ifdef DEBUG
+  Serial.begin(115200);
+#endif
 }
 
 void loop() {
-  //read values from pin 2 & 3 (PIND = pins 0-7)
-  //shift pin state right: 10101100 -> 101011
-  //mask state of pins with 3 (11 in binary): 101011 -> 000011
-  int val = (PIND >> 2) & 0x3;
-  int valForAvg = 0;
+  uint8_t pinVal = val;
+  uint16_t valForAvg = 0;
 
-  if (digitalRead(2) || digitalRead(3)) { 
-    digitalWrite(13,HIGH);
-  } else {
-    digitalWrite(13,LOW);
-  }
-  
-  if (val != _val){
-    int tableIndex = (_val << 2 | val);
-    Serial.println("Change! val:" + String(val));
-    count += table[tableIndex];
-    _val = val;
+  if (pinVal != _val) {
     valForAvg = 1;
+    _val = val;
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
   }
-  
-//  avg[index] = valForAvg;
-//  index = (index + 1) % smooth;
+
+
+#ifdef DEBUG
+  //  Serial.print(0);
+  //  Serial.print(",");
+  //  Serial.print(127);
+  //  Serial.print(",");
+//  Serial.println(val);
+#endif
+
+  avg[i] = valForAvg;
+  i = (i + 1) % smooth;
 
   float sum = 0;
-  for (int i = 0; i < smooth; i++){
+  for (int i = 0; i < smooth; i++) {
     sum += avg[i];
   }
 
-  midiOut = floor((sum / smooth) * 127);
+  midiOut = floor(scalingTable[int(floor(sum / smooth * 127))] * (sum / smooth) * 127);
+  midiSmooth[midiSmoothIndex] = midiOut;
+  midiSmoothIndex = (midiSmoothIndex + 1) % midiSmoothCount;
 
-  if (midiOut != _midiOut){
-    MIDI.sendControlChange(80, midiOut, 1);
+  float midiSum = 0;
+
+  for (int i = 0; i < midiSmoothCount; i++) {
+    midiSum += midiSmooth[i];
+  }
+
+  uint8_t midiSmoothedOut = floor(midiSum / midiSmoothCount);
+
+#ifdef DEBUG
+  //    Serial.println(midiSmoothedOut);
+#endif
+
+  if (midiOut != _midiOut) {
+    sinceTouched = 0;
+    usbMIDI.sendControlChange(80, midiOut, 1);
     _midiOut = midiOut;
   }
 
-  
+  setLEDs(midiOut);
+
+  if (usbMIDI.read()) {
+    processMIDI();
+  }
+
+  delayMicroseconds(700);
+
+
+  checkIdle();
+}
+
+void encoderIRQ(void) {
+  val = (PIND >> 2) & 0x3;
+
+  int tableIndex = (_val << 2 | val);
+  count += table[tableIndex];
 }
